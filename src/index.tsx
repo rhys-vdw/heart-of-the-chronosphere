b7435d9cc24adb0e94bfc63c7a79f2eda1eb0ff8
Rhys van der Waerden <rhys.vdw@gmail.com>
2020-02-29 15:31:39 +0100

    Disable preact debugger
    Causing error in console

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 if (typeof window !== "undefined" && process.env.NODE_ENV === "development") {
   // tslint:disable-next-line:no-var-requires
-  require("preact/debug");
+  // require("preact/debug");
 }
 
 import { render, h } from "preact";
@@ -43,3 +43,35 @@ html, body, button {
 `;
 
 render(<Main />, document.getElementById("root")!);
+
+// import * as THREE from "three";
+// // tslint:disable-next-line:no-duplicate-imports
+// import { Scene, WebGLRenderer, Vector2, PerspectiveCamera } from "three";
+
+// // const canvas = this.canvasRef.current!;
+// const scene = new Scene();
+
+// // const bounds = canvas.getBoundingClientRect();
+// const camera = new PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+// const renderer = new WebGLRenderer();
+// renderer.setSize(window.innerHeight, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+// camera.position.z = 5;
+
+// const animate = () => {
+//   requestAnimationFrame(animate);
+//   renderer.render(scene, camera);
+// };
+// animate();
+
+// this.redraw();
