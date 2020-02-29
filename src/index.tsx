996085da015dc1ef90befe3f31b017b207910b27
Rhys van der Waerden <rhys.vdw@gmail.com>
2020-02-29 16:29:29 +0100

    Remove commented out code

--- src/index.tsx
+++ src/index.tsx
@@ -43,35 +43,3 @@ html, body, button {
 `;
 
 render(<Main />, document.getElementById("root")!);
-
-// import * as THREE from "three";
-// // tslint:disable-next-line:no-duplicate-imports
-// import { Scene, WebGLRenderer, Vector2, PerspectiveCamera } from "three";
-
-// // const canvas = this.canvasRef.current!;
-// const scene = new Scene();
-
-// // const bounds = canvas.getBoundingClientRect();
-// const camera = new PerspectiveCamera(
-//   75,
-//   window.innerWidth / window.innerHeight,
-//   0.1,
-//   1000
-// );
-// const renderer = new WebGLRenderer();
-// renderer.setSize(window.innerHeight, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-// camera.position.z = 5;
-
-// const animate = () => {
-//   requestAnimationFrame(animate);
-//   renderer.render(scene, camera);
-// };
-// animate();
-
-// this.redraw();
