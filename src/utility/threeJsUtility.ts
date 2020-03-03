5c731364b02a06d19e1679584534d28e8380ffdd
Rhys van der Waerden <rhys.vdw@gmail.com>
2020-03-03 13:51:44 +0100

    Optional z value in vec2to3

--- src/utility/threeJsUtility.ts
+++ src/utility/threeJsUtility.ts
@@ -4,6 +4,6 @@ export function vec3to2({ x, y }: Vector3): Vector2 {
   return new Vector2(x, y);
 }
 
-export function vec2to3({ x, y }: Vector2): Vector3 {
-  return new Vector3(x, y, 0);
+export function vec2to3({ x, y }: Vector2, z = 0): Vector3 {
+  return new Vector3(x, y, z);
 }
